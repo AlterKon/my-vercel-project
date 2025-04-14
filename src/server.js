07f458b09c83064991279d70023f6cbd5e5b7910
AlterKon <nguyenchanhcong12@gmail.com>
2025-04-14 18:59:06 +0700

    update

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require('express');
 const session = require('./config/session');
 const cors = require('cors');
@@ -11,29 +10,16 @@ const configViewEngine = require('./config/viewengine');
 
 const app = express();
 
-// Middleware chung
 app.use(session);
 app.use(setLocals);
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'src', 'public')));
+
 configViewEngine(app);
 
-// Routes
 app.use('/', publicRoutes);
 app.use('/user', userRoutes);
 app.use('/admin', adminRoutes);
-app.get('/test', (req, res) => {
-    res.send('Test route is working!');
-});
 
-app.use((req, res, next) => {
-    res.status(404).send('Page not found');
-  });
-
-// Start
-const port = process.env.PORT || 8888;
-app.listen(port, () => {
-    console.log(`App is listening on port ${port}`);
-});
+module.exports = app;
