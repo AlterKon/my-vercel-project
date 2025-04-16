a14d94dc686691f28e7fe901d0b3df02bec1a139
AlterKon <nguyenchanhcong12@gmail.com>
2025-04-16 17:34:51 +0700

    new update 16/4

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -3,6 +3,8 @@ const cloudinary = require('cloudinary').v2;
 const path = require('path');
 require('dotenv').config();
 
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
